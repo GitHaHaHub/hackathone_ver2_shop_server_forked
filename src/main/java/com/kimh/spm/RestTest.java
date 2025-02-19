26cb28b99cd781741a9ac1beec96b0913d315bc6
GitHaHaHub <92131005+GitHaHaHub@users.noreply.github.com>
2025-02-19 17:02:17 +0900

    feat: created DB connection and modified RestTest to return all the users in the DB (#2)
    Resolves #SPMA-4

--- src/main/java/com/kimh/spm/RestTest.java
+++ src/main/java/com/kimh/spm/RestTest.java
@@ -3,11 +3,20 @@ package com.kimh.spm;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import com.kimh.spm.model.User;
+import com.kimh.spm.service.UserHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class RestTest {
+
+    @Autowired
+    private UserHandler userHandler;
+
     @GetMapping("/RestTest")
-    public String home() {
-        return "Hello, RestTest!";
+    public ResponseEntity<List<User>> viewAllUsers() {
+        return ResponseEntity.ok(userHandler.allUsers());
     }
 }
